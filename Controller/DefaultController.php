1c207fddabbf56eaf88040d62b46ca97e6cc29a5
JAY David <dajay@sqli.com>
2013-08-28 14:30:25 +0200

    create generic route datatables/{entity}

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -6,14 +6,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Level42\Bundle\DataTablesBundle\DataTables\Result\DataTablesResultDoctrine;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @Route("/datatables")
+ */
 class DefaultController extends Controller
 {
+    
     /**
-     * @Route("/hello/{name}")
-     * @Template()
+     * @Route("/{entity_class}", name="_datatables_entity")
+     * @param DataTablesDoctrine $data
+     * @return result
      */
-    public function indexAction($name)
+    public function entityAction(DataTablesResultDoctrine $data)
     {
-        return array('name' => $name);
-    }
+    	$data = $this->get('jms_serializer')->serialize($data, "json");
+    	return Response::create($data,200,array('Content-Type'=>"application/json"));
+    }    
+    
 }
